3736a427e42d862911b5cb6cf95021db5b33aef9
hansintheair <hannesz1@gmail.com>
2021-01-01 19:38:04 -0800

    simulate food, eat, repro., & org. death

--- Section_3/simpop.py
+++ Section_3/simpop.py
@@ -14,10 +14,10 @@ def trial(chance):
     """ Returns the boolean outcome of a trial given it's chance of success."""
     return random() <= chance
 
-def distance(source, other):
+def distance(source, target):
     """ Returns the distance between two Entity objects."""
-    dx = source.x - other.x
-    dy = source.y - other.y
+    dx = source.x - target.x
+    dy = source.y - target.y
     return hypot(dx, dy)
 
 def collision(source, target):
@@ -26,24 +26,34 @@ def collision(source, target):
         return True
     return False
 
-def resolve_collision(source, target):
+def resolve_organism_collision(source, target):
     """ Simple collision resolution wherein source and target entities
         trade vector.
     """
-    source.motion, target.motion = target.motion, source.motion    
+    source.motion, target.motion = target.motion, source.motion
+
+def resolve_border_collision(source, extent):
+    """ ."""
+    if (source.x >= (extent.width - source.radius) or 
+        source.x <= (0 + source.radius)
+        ):
+        source.motion.vx = -source.motion.vx
+    if (source.y >= (extent.height - source.radius) or 
+        source.y <= (0 + source.radius)
+        ):
+        source.motion.vy = -source.motion.vy
 
 def resolve_overlap(source, target):
     """ Resolve the overlap between two entities such that the target entity
         and the source entity positions are adjusted to the position along
         their respective vectors at which the entities make first contact.
     """
-
     v_st = Vector(source.x - target.x, source.y - target.y)
-    enforce_distance = source.radius + target.radius
-    overlap_distance = enforce_distance - v_st.mag
+    touch_distance = source.radius + target.radius
+    overlap_distance = touch_distance - v_st.mag
 
-    source += v_st.unit * round((source.radius / enforce_distance) * overlap_distance, 5)
-    target += -v_st.unit * round((target.radius / enforce_distance) * overlap_distance, 5)
+    source += v_st.unit * round((source.radius / touch_distance) * overlap_distance, 5)
+    target += -v_st.unit * round((target.radius / touch_distance) * overlap_distance, 5)
 
 
 class Extent():
@@ -60,28 +70,45 @@ class Extent():
                 entity.y <= (self.height - entity.radius) and
                 entity.y >= (0 + entity.radius))
 
+class Point():
+    """ ."""
 
-class Entity(ABC):
-    """ Abstract base class for entities. An entity is any interactable object in 
-        the Environment that has location (x, y) and radius.
-    """
-
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = radius
-        self.patch = plt.Circle((x, y), radius) # matplotlib patch
-    
+
     def __iadd__(self, vector):
         """  Update the location of self by Vector."""
         self.x = self.x + vector.vx
         self.y = self.y + vector.vy
         return self
-    
+
+class Entity(ABC, Point):
+    """ Abstract base class for entities. An entity is any interactable object in 
+        the Environment that has location (x, y) and radius.
+
+        Parameters:
+            x (int or float):                   x location of Entity.
+            y (int or float):                   y location of Entity.
+            radius (float):                     the radius of the Entity.
+            energy (float):                     the amount of energy stored by Entity.
+        Attributes:
+            patch (matplotlib.patches.Circle):  patch for visualization on plot.
+            alive (bool):                       alive if True, dead if False.
+    """
+
+    def __init__(self, x, y, radius, energy=1):
+        super().__init__(x, y)
+        self.radius = radius
+        self.energy = energy
+        self.patch = plt.Circle((x, y), radius) # matplotlib patch
+        self.alive = True
+   
     @staticmethod
     @abstractmethod
     def spawn(extent):
-        """ Every child of Entity must have a static spawn method."""
+        """ Every child of Entity must have a static spawn method
+            that accepts a required extent parameter."""
         pass
 
 
@@ -92,18 +119,28 @@ class Organism(Entity):
             x (int or float):                   x location of Organism.
             y (int or float):                   y location of Organism.
             radius (float):                     the radius of the Organism.
+            energy (float):                     the amount of energy stored by Organism.
             motion (Vector):                    the speed and direction of Organism.
         Attributes:
-            period (int):                       the current age of Organism.
-            energy (int or float):              the amount of energy stored by Organism.      
             patch (matplotlib.patches.Circle):  patch for visualization on plot.
+            alive (bool):                       alive if True, dead if False.
+            period (int):                       the current age of Organism.
+            reproduce_thresh (float):           can reproduce if energy greater.
+            reproduce_chance (float):           chance of reproduction.
+            reproduce (bool):                   duplicate if True else don't.
+            consumption (float):                energy consumed per period.
+
     """
     
-    def __init__(self, x, y, radius = .25, motion = Vector(0, 0)):
+    def __init__(self, x, y, radius =0.25, motion=Vector(0, 0)):
         super().__init__(x, y, radius)
         self.motion = motion
-        self.energy = 2
         self.period = 0
+        self.reproduce_thresh = 2
+        self.reproduce_chance = 0.25
+        self.reproduce = False
+        self.consumption = 0.05
+
 
     def __str__(self):
         return f"Organism <age: {self.period!r}, location({self.x!r}, {self.y!r})>"
@@ -113,10 +150,33 @@ class Organism(Entity):
     
     def __call__(self):
         """ advance the entity by one period."""
-        self += self.motion
         self.period += 1
+        self += self.motion
+        self.energy -= self.consumption
+        if self.energy <= 0:
+            self.alive = False
+        elif self.energy > self.reproduce_thresh:
+            if trial(self.reproduce_chance):
+                self.reproduce = True
         self.patch.center = (self.x, self.y)
         return self
+
+    def eat(self, food):
+        food.alive = False
+        self.energy += food.energy
+
+    def duplicate(self, extent):
+        self.reproduce = False
+        self.energy -= 1
+        touch_distance = self.radius * 2
+        vector = Vector.generate(1).unit * round(touch_distance, 5)
+        loc = Point(self.x, self.y)
+        loc += vector
+        return Organism(
+            x=loc.x,
+            y=loc.y,
+            motion=Vector.generate(0.15)
+        )
 
     @staticmethod
     def spawn(extent):
@@ -137,11 +197,11 @@ class Food(Entity):
             energy (float):                     The energy value of Food.
         Attributes:
             patch (matplotlib.patches.Circle):  patch for visualization on plot.
+            alive (bool):                       alive if True, dead if False.
     """
 
-    def __init__(self, x, y, radius = .1, energy = 1):
+    def __init__(self, x, y, radius=0.1):
         super().__init__(x, y, radius)
-        self.energy = energy
         self.patch.set_color("green")
     
     def __repr__(self):
@@ -159,29 +219,24 @@ class Environment():
     """ The environment tracks the population and parameters of the simulation.
 
         Parameters:
-            repro_chance (float):               Chance of reproduction
-            death_chance (float):               Chance of death
             starting_pop (int):                 Starting population
             extent (iterable [width, height]):  Extent of habitat
         
         TODO: Explain remaining attributes once fully established.
     """
 
-    def __init__(self, repro_chance, death_chance, starting_pop, starting_food, food_rate, extent):
+    def __init__(self, starting_pop, starting_food, food_rate, extent):
         # Simulation arguments
-        self.repro_chance = repro_chance
-        self.death_chance = death_chance
         self.starting_pop = starting_pop
         self.starting_food = starting_food
         self.food_rate = food_rate
         self.extent = Extent(*extent)
-        # Hard simluation parameters
-        self.period_length = 25
+        self.period = 0
         # Simulation data
-        self.ents = Entities(population=[], food=[]) #Currently existing entities
-        for i in range(0, self.starting_pop):
+        self.ents = Entities(population=[], food=[]) # entities existing at start of simulation 
+        for _ in range(0, self.starting_pop):
             self.ents.population.append(self.spawn(Organism))
-        for i in range(0, self.starting_food):
+        for _ in range(0, self.starting_food):
             self.ents.food.append(self.spawn(Food))
         self.history = [starting_pop]
         self.mortuary = Counter()
@@ -197,39 +252,49 @@ class Environment():
     def __call__(self):
         """ Advance the simulation by one frame."""
         # Simulate period
-        # survive, expire, births = [], [], []
-
+        
+        self.period += 1
+        
         # Update organism motion
-        queue = deque(self.ents.population)
-        while len(queue) > 0:
+        collision_queue = deque(self.ents.population)
+        for organism in self.ents.population:
+            collision_queue.remove(organism)
             # Organism collides with extent boundary
-            organism = queue.pop()
-            if (organism.x >= (self.extent.width - organism.radius) or 
-                organism.x <= (0 + organism.radius)
-               ):
-                organism.motion.vx = -organism.motion.vx
-            if (organism.y >= (self.extent.height - organism.radius) or 
-                organism.y <= (0 + organism.radius)
-               ):
-                organism.motion.vy = -organism.motion.vy
+            resolve_border_collision(organism, self.extent)
             # organism collides with other organsim
-            targets = (target for
-                            target in
-                            queue if
-                            target is not organism and collision(organism, target)
-                            )
-            for target in targets:
-                resolve_collision(organism, target)
-                resolve_overlap(organism, target)
-
-            # Update organism position
+            for target in collision_queue:
+                if collision(organism, target):
+                    resolve_organism_collision(organism, target)
+                    resolve_overlap(organism, target)
+            # organism collides with food
+            for target in self.ents.food:
+                if collision(organism, target):
+                    organism.eat(target)
+            # Update organism
             organism()
+        
+        # Update deaths and births
+        mortuary, offspring = [], []
+        for organism in self.ents.population:
+            if not organism.alive:
+                mortuary.append(organism.period)
+                self._ax.patches.remove(organism.patch)
+                self.ents.population.remove(organism)
+        for organism in self.ents.population:
+            if organism.reproduce:
+                child = self.duplicate(organism)
+                offspring.append(child)
+                
 
-            
-        # # Finally, update attributes
-        # self.ents.population = survive + births
-        # self.mortuary.update(expire)
-        # self.history.append(len(self.population))
+        # Update food eaten
+        eaten = [food for food in self.ents.food if not food.alive]
+        for food in eaten:
+            self._ax.patches.remove(food.patch)
+            self.ents.food.remove(food)
+
+        # update data
+        self.mortuary.update(mortuary)
+        self.ents.population + offspring
 
     def fig_init(self):
         for group in self.ents:
@@ -245,34 +310,24 @@ class Environment():
                                        init_func=self.fig_init,
                                        frames=n_period)
 
-
-        ##TODO
-        ##PSEUDOCODE##
-        """ 
-            for each iteration in range(0, n_iterations):  #how many iterations to simulate total  
-                for each interval range(0, interval_size):
-                    for each cell in population:
-                        Cell moves and gathers food
-                            if cell energy <= 0:
-                                cell stops moving
-                    Update graphic
-                -Evaluate cell success after interval-
-                for each cell in population:
-                    if cell energy <= 0:
-                        remove cell from population and add to mortuary
-                    if cell energy >= repro_energy_requirement:
-                        add new cell to population, offset from original cell
-                
-                Update population line graph
-                Update popualtion expiration histogram
-        """
-
     def spawn(self, entity):
         while True:
             ent = entity.spawn(self.extent)
             # Must spawn inside of habitat and must not overlap with others
             if self.extent.contains(ent):
-                if not any(collision(ent, target) for group in self.ents 
+                if not any(collision(ent, target) for group in self.ents
                            for target in group):
-                    return ent                
-        
+                    return ent
+    
+    def duplicate(self, entity):
+        while True:
+            ent = entity.duplicate(self.extent)
+            # Must spawn inside of habitat and must not overlap with others
+            if self.extent.contains(ent):
+                if not any(collision(ent, target) for group in self.ents
+                           for target in group):
+                    self.ents.population.append(ent)
+                    self._ax.add_patch(ent.patch)
+                    return ent
+                    
+                    
